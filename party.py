1edc46646ec31a17e3b1997bffffe63bc981773b
dianasch <dianaschao@gmail.com>
2020-10-19 16:29:22 +0000

    Adds doctest to most_and_least_common_type function.
    Co-authored-by: kayteecannon <kaytee.cannon@me.com>

--- party.py
+++ party.py
@@ -26,6 +26,17 @@ def most_and_least_common_type(treats):
     """Given list of treats, return most and least common treat types.
 
     Return most and least common treat types in tuple of format (most, least).
+        >>> treats = [
+        ...     {'type': 'dessert'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'dessert'},
+        ...     {'type': 'appetizer'},
+        ...     {'type': 'drink'},
+        ... ]
+
+        >>> most_and_least_common_type(treats)
+        ('dessert', 'drink')
     """
 
     types = {}
